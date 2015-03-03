761f03250d029a052f697781159912abd1dbb4d3
Pablo Rodríguez Valdés <valdes86@gmail.com>
2015-03-03 09:56:28 +0100

    Update MenuOptions.java

--- extract/src/main/java/es/uniovi/asw/trivial/MenuOptions.java
+++ extract/src/main/java/es/uniovi/asw/trivial/MenuOptions.java
@@ -18,22 +18,20 @@ public class MenuOptions {
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. GIFT-JSON      |");
 		System.out.println("|        2. XML-JSON       |");
-		System.out.println("|        3. Exit           |");
+		System.out.println("|        3. Salir         |");
 		System.out.println("============================");
 		opcion=scanner.nextInt();
 
 		switch (opcion) {
 		case 1:
-			System.out.println("Option 1 selected"); // This is where I want to
-														// call the class
+			System.out.println("Option 1 selected"); 
 			GIFTParser jsonParser = new GIFTParser();
 			System.out.println("parsing...");
 			jsonParser.parserGIFT();
 			System.out.println("done!");
 			break;
 		case 2:
-			System.out.println("Option 2 selected"); // this is where I want to
-														// call the class
+			System.out.println("Option 2 selected"); 
 			XMLParser xmlParser = new XMLParser();
 			xmlParser.parseXMLFile("Data/questions");
 			break;
