26d9b233be8de3e22c9e7189dccb4a43a1b6f422
unknown <uo219674@EII-L-01-03.aulas.uniovi.es>
2015-03-17 09:41:58 +0100

    Cambios en el test para que funcione travis.

--- extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
+++ extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
@@ -2,12 +2,35 @@ package es.uniovi.asw.trivial;
 
 import java.io.IOException;
 
+import org.w3c.dom.Document;
+
+import com.google.gson.Gson;
+
+import es.uniovi.asw.parsers.json.GIFTParser;
+import es.uniovi.asw.parsers.xml.XMLParser;
+import es.uniovi.asw.util.Question;
+
 public class Extractor {
 	
 	public void usage() throws IOException {
 		System.out.println("Wellcome to Trivial Extractor");
-		MenuOptions menu = new MenuOptions();
-		menu.start();
+
+		System.out.println("Resultado de parsear las preguntas en formato GIFT");
+		GIFTParser jsonParser = new GIFTParser();
+		for(Question q:jsonParser.parserGIFT("Data/questionsGIFT")){
+			Gson gson = new Gson();
+			String representacionJSON = gson.toJson(q);
+			System.out.println(representacionJSON);
+		}
+		
+		XMLParser xmlParser = new XMLParser();
+		System.out.println("Resultado de parsear las preguntas en formato XML");
+		Document doc = xmlParser.parseXMLFile("Data/questionsXML.xml");
+		for(Question q:xmlParser.parseDocument(doc)){
+			Gson gson = new Gson();
+		String representacionJSON = gson.toJson(q);
+		System.out.println(representacionJSON);
+		}
 		
 	}
 	public int run(String[] args) throws IOException {
