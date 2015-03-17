4c730b8c5c0b60a8d37c454f1e33891da203cd42
labra <jelabra@gmail.com>
2015-03-17 10:29:58 +0100

    Incluidas las historias de usuario

---
+++ game/src/main/java/es/uniovi/asw/App.java
@@ -0,0 +1,13 @@
+package es.uniovi.asw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
