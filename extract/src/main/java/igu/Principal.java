64e788bd2fc73959bee7cbb53d571b7c02b46941
carlacs <carlacs90@gmail.com>
2015-03-30 19:00:37 +0200

    Añadidos ficheros de interfaz a Extract

---
+++ extract/src/main/java/igu/Principal.java
@@ -0,0 +1,14 @@
+package igu;
+
+import javax.swing.JFrame;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		VentanaInicio v = new VentanaInicio();
+		v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		v.setVisible(true);
+	}
+
+}
