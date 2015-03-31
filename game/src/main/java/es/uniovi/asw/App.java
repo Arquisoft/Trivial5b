7d3f0586bb53d89c20c2cf781f228c4f31cdcd75
valdes86 <valdes86@gmail.com>
2015-03-31 18:15:44 +0200

    Cambiado igu logica e imagenes a Game y ahora funciona desde la clase App

--- game/src/main/java/es/uniovi/asw/App.java
+++ game/src/main/java/es/uniovi/asw/App.java
@@ -1,5 +1,7 @@
 package es.uniovi.asw;
 
+import igu.Principal;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Principal.main(null);
+    	
+        System.out.println( "App funcionando..." );
     }
 }
